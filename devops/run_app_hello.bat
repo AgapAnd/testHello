aff5b93655a952038f8c3ae5cefccebf37295478
AgapAnd <braniganzepp@gmail.com>
2024-10-08 09:17:37 +0300

    modify run_app_hello
    Conditions of existing files Hello.java and Hello.class were added in the file.

--- devops/run_app_hello.bat
+++ devops/run_app_hello.bat
@@ -1,17 +1,33 @@
 @echo off
-
 echo "Hello World" application
 
-echo Step 1. Compiling...
 
-cd ..
+echo Step 1. Checking file...
 
-cd application
+cd ../application
+
+if not exist src/ru/agapov/app/Hello.java (
+	echo File "Hello.java" not found
+	goto end
+)
+
+if exist bin/ru/agapov/app/Hello.class (
+	goto running
+)
+
+
+echo Step 2. Compiling...
 
 javac -d bin src/ru/agapov/app/Hello.java
 
-echo Step 2. Programm is running...
+
+
+:running
+echo Step 3. Programm is running...
 
 java -classpath ./bin ru.agapov.app.Hello
 
+
+
+: end
 pause
